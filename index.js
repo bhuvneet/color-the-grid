8aa931dfcf31734168b96fd8ba6a607e1ad94269
bhuv3 <bhuvneet3@gmail.com>
2022-06-06 04:46:00 -0400

    Created 16x16 grid and styled basic blue solid border, displayed as grid.

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
+// container div to hold all rows and column divs
 let grid = document.getElementById("grids");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("gridCells");
 
-let cell = document.createElement("div");
-cell.innerHTML = "HTML";
-grid.appendChild(cell);
-
+// create the defaul 16x16 grid by invoking rowsColumns function.
+function grid16by16 ()
+{
+    // 256 cells in total
+    let numOfrows = 16;
+    rowsColumns(numOfrows);
+}
 
 // get userInput to create rows and column grid less than 100.
-function rowsColumns (numOfCells)
+function rowsColumns (numOfrows)
 {
-    if (numOfCells > 100)
+    if (numOfrows > 100)
     {
         alert("Make another choice less than 100 cells");
     }
     else
     {
-        for(let i = 0; i < numOfCells; i++)
+        for(let i = 0; i < numOfrows; i++)
         {
             let row = document.createElement("div");
             grid.appendChild(row).className = "gridRow";
-    
-            let column = document.createElement("div");
-            grid.appendChild(column).className = "gridColumn";
+            
+        }
+
+        for(let j = 0; j < (numOfrows * numOfrows); j++)
+        {
+            let cell = document.createElement("div");
+            grid.appendChild(cell).className = "gridCells";
         }
     }
 }
+
+grid16by16();
