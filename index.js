6f9bf1b21a849f9fd7ba4659f5362fc375236752
bhuv3 <bhuvneet3@gmail.com>
2022-06-06 03:21:36 -0400

    Created a function rowsColumn() for creating a grid per user input.

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ let grid = document.getElementById("grids");
 let cell = document.createElement("div");
 cell.innerHTML = "HTML";
 grid.appendChild(cell);
+
+
+// get userInput to create rows and column grid less than 100.
+function rowsColumns (numOfCells)
+{
+    if (numOfCells > 100)
+    {
+        alert("Make another choice less than 100 cells");
+    }
+    else
+    {
+        for(let i = 0; i < numOfCells; i++)
+        {
+            let row = document.createElement("div");
+            grid.appendChild(row).className = "gridRow";
+    
+            let column = document.createElement("div");
+            grid.appendChild(column).className = "gridColumn";
+        }
+    }
+}
