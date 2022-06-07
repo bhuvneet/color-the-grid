25cf42d9d3d2e48cd16fb3bc01618023d4ea3a62
bhuv3 <bhuvneet3@gmail.com>
2022-06-07 05:21:30 -0400

    Fixed displaying of grid, by removing createColumns() and added logic of creating columns to the CreateGrid(). Added gridTemplate style in CreateGrid function to display row and cells as a grid. Fixed CSS styling for cells to be responsive squares as brower's maximises and minimises.

--- index.js
+++ index.js
@@ -1,38 +1,87 @@
 // container div to hold all rows and column divs
-let grid = document.getElementById("grids");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("gridCells");
+let container   = document.getElementById("container");
+let rows        = document.querySelectorAll("gridRow");
+let cells       = document.querySelectorAll("gridCells");
 
 // create the defaul 16x16 grid by invoking rowsColumns function.
 function grid16by16 ()
 {
     // 256 cells in total
-    let numOfrows = 16;
-    rowsColumns(numOfrows);
+    let numOfRows = 10;
+    createRows(numOfRows);
 }
 
 // get userInput to create rows and column grid less than 100.
-function rowsColumns (numOfrows)
+function createRows (numOfRows)
 {
-    if (numOfrows > 100)
-    {
-        alert("Make another choice less than 100 cells");
+    for(let i = 0; i < numOfRows; i++)
+    {   
+        // create rows
+        let row = document.createElement("div");
+        row.id = "gridRow";  
+        container.appendChild(row);
     }
-    else
+    
+    let numOfCells = (numOfRows*numOfRows) - numOfRows;
+    for(let j = 0; j < numOfCells; j++)
     {
-        for(let i = 0; i < numOfrows; i++)
-        {
-            let row = document.createElement("div");
-            grid.appendChild(row).className = "gridRow";
-            
-        }
+        let cell = document.createElement("div");
+        cell.id = "gridCells";
+        container.appendChild(cell);
+    }
+    
+    let cellPerColumn = numOfRows - 1;
 
-        for(let j = 0; j < (numOfrows * numOfrows); j++)
-        {
-            let cell = document.createElement("div");
-            grid.appendChild(cell).className = "gridCells";
-        }
+    //createColumns(numOfRows);
+    document.getElementById("container").style.gridTemplate = "repeat(" + cellPerColumn + ", 1fr) / repeat(" + numOfRows + ", 1fr)";
+    //document.getElementById("container").style.gridTemplateColumns = "repeat(" + cellPerColumn + ", 1fr)";
+}
+/*
+function createColumns (numOfRows)
+{
+    // append cells to the rows
+    for(let j = 0; j < (numOfRows*numOfRows); j++)
+    {
+        let cell = document.createElement("div");
+        cell.id = "gridCells";
+        container.appendChild(cell);
     }
 }
 
+function newGrid ()
+{
+    let userValue = document.getElementById("userCreatedGrid").value;
+    console.log(userValue);
+
+    // prompt user to enter number of rows less than 100
+    if (userValue > 100)
+    {
+        let gridText = document.getElementById("newGridText");
+        gridText.textContent = "Enter a new number for rows, less than 100: ";
+        let clear = document.getElementById("userCreatedGrid").value ='';
+    }
+    // create grid for the number of rows entered by user
+    else
+    {
+        // remove divs inside the grid
+        removePrevGrid();
+
+        // invoke rowsColumns function to create new grid
+        createRows(userValue);
+    }
+}
+
+function removePrevGrid ()
+{
+    const prevGRid = document.getElementById("grids");
+    
+    while (prevGRid.hasChildNodes())
+    {
+        prevGRid.removeChild(prevGRid.firstChild);
+    }
+
+    return;
+}
+*/
 grid16by16();
+
